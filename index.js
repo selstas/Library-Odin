bd23cbfb0c57730878cf99bfdbc9dac2fd503401
selstas <xoxloslav@ukr.net>
2022-12-22 17:00:48 +0200

    init

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 let myLibrary = []; 
 
 // dfk for what i needed it
-function Book (Title, Author, Pages, Read) {
+class Book {
+  constructor(Title, Author, Pages, Read) {
   this.Title = Title;
   this.Author = Author;
   this.Pages = Pages;
@@ -10,6 +11,7 @@ function Book (Title, Author, Pages, Read) {
   /*this.info = function() {
     return (title + " ," + author + " ," + pages + " ," + "Have read yet: " + isRead)
   } */
+}
 }
 
 function addBookToLibrary(Title, Author, Pages, Read) {
@@ -21,18 +23,45 @@ function addBookToLibrary(Title, Author, Pages, Read) {
 //function to display librrary to cards
 function displayLibrary() {
   const books = document.querySelector(".books");
-  
 
+  const removeDivs = document.querySelectorAll(".card");
+  for (let i = 0; i < removeDivs.length; i++) {
+      removeDivs[i].remove();
+  } 
  // loop over the array and diplay it in a card
+  let index = 0;
   myLibrary.forEach(myLibrary => {
     const card = document.createElement("div");
     card.classList.add("card");
     books.appendChild(card);
-      for(let key in myLibrary) {
-        console.log(`${key}: ${myLibrary[key]}`);
-        const para = document.createElement("p");
-        para.textContent = (`${key}: ${myLibrary[key]}`);
-        card.appendChild(para);
+
+
+        const removeBookButton = document.createElement("button");
+        removeBookButton.classList.add("remove-Book-Button");
+        removeBookButton.textContent = "Remove From Library";
+        console.log("show me current array object inside foreach....", myLibrary);
+
+        removeBookButton.dataset.linkedArray = index;
+        index++;
+        console.log("show me dataset", removeBookButton.dataset.linkedArray);
+        card.appendChild(removeBookButton);
+
+        removeBookButton.addEventListener("click", removeBookFromLibrary);
+
+        function removeBookFromLibrary () {
+          let retriveBookToRemove = removeBookButton.dataset.linkedArray;
+          console.log("attemp", parseInt(retriveBookToRemove));
+          console.log( myLibrary);
+          myLibrary.splice(parseInt(retriveBookToRemove), 1);
+          card.remove();
+          displayLibrary();
+        }
+
+    for(let key in myLibrary) {
+      console.log(`${key}: ${myLibrary[key]}`);
+      const para = document.createElement("p");
+      para.textContent = (`${key}: ${myLibrary[key]}`);
+      card.appendChild(para);
       }
   })
 
